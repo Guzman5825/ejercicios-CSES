58584803bffb775689675e91e3c4bcef1a888c87
edi <guzmanedi5825@gmail.com>
2024-04-23 16:49:50 -0300

    a

--- introductory problems/Bit String.py
+++ introductory problems/Bit String.py
@@ -1,5 +1,5 @@
 MOD = 10**9 + 7
 
 N=int(input())
-#en python no hay problema, pero si en otro lenguaje por el int-> long long
+#en python no hay problema, pero si en otro lenguaje por el int
 print( (2**N)%MOD ) 
