16fa4bc6e3eed6ecea454780e1ef7766389986b1
edi <guzmanedi5825@gmail.com>
2024-04-23 16:51:37 -0300

    a

--- introductory problems/Trailing Zeros.py
+++ introductory problems/Trailing Zeros.py
@@ -1,5 +1,6 @@
-#
-
+#la solucion de este problema es econtrar los multiplos de 5,25(5^2),125(5^3)
+#ya que siempre estos multiplos al multiplicar le daran un 0 extra al final
+#no se usa factorial por que el coste N=10**9 es muy costoso (mas de 1 minuto)
 N=int(input())
 cantCeros=0
 
